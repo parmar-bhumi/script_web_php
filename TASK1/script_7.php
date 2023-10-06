66a48623be11883b60c17d3743eaa9a595c14ad4
bhumi parmar <143687461+parmar-bhumi@users.noreply.github.com>
2023-10-06 17:24:31 +0530

    Add files via upload
    Script_7: Write a PHP script to swap the values of two variables $x and $y using a temporary
    variable. Display the swapped values using the echo statement.

---
+++ TASK1/script_7.php
@@ -0,0 +1,16 @@
+<?php
+$x=25;
+$y=45;
+echo "<br>"."before swapped";
+echo "<hr>";
+echo "first number=".$x;
+echo "<br>"."second number=".$y;
+echo "<br>";
+$temp=$x;
+$x=$y;
+$y=$temp;
+echo "<br>"."after swapped";
+echo "<hr>";
+echo "first number=".$x;
+echo "<br>"."second number=".$y;
+?>
