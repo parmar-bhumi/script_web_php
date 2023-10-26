9d38d888a32e5c9b40343578b74079f91f4f16bd
bhumi parmar <143687461+parmar-bhumi@users.noreply.github.com>
2023-10-26 18:44:40 +0530

    Add files via upload
    Script_34 Develop a PHP script to check if a specific value exists in an array using the in_array()
    function.

---
+++ Task_3/script_34.php
@@ -0,0 +1,12 @@
+<?php
+$people = array("Peter", "Joe", "Glenn", "Cleveland");
+
+if (in_array("Glenn", $people))
+  {
+  echo " This is match ";
+  }
+else
+  {
+  echo " This is not match ";
+  }
+?>
